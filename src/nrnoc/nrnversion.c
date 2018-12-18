b00d2ba6025a7cfd25647af7b3716bc8ebd3af3b
Judit <jplanasc@users.noreply.github.com>
2018-12-18 14:53:30 +0100

    Update branch name in nrnversion.c
    This is a temporary change, just to make sure when we are using the ime-fix branch.

--- src/nrnoc/nrnversion.c
+++ src/nrnoc/nrnversion.c
@@ -17,7 +17,7 @@ static char configargs[] = NRN_CONFIG_ARGS;
 
 #if !defined(GIT_BRANCH)
 #define GIT_DATE "2018-08-24"
-#define GIT_BRANCH "unknown"
+#define GIT_BRANCH "ime-fix"
 #define GIT_CHANGESET "d3ead4a+"
 #define GIT_DESCRIBE "7.6.2-2-gd3ead4a+"
 #endif
